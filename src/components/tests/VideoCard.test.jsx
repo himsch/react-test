1a1db526216a08b78fb9e28de771a6f88985017e
song <55rsearchco@gmail.com>
2023-03-29 12:31:38 +0900

    비디오 카드 테스트 - 동적

--- src/components/tests/VideoCard.test.jsx
+++ src/components/tests/VideoCard.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
 import VideoCard from '../VideoCard/VideoCard';
 import '@testing-library/jest-dom';
 import { formatAgo } from '../../util/date';
+import userEvent from '@testing-library/user-event';
 
 describe('VideoCard', () => {
   const video = {
@@ -35,5 +36,26 @@ describe('VideoCard', () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+  it('navigates to detailed video page with video state when clicked', () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+    render(
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const card = screen.getByRole('listitem');
+    userEvent.click(card);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });
